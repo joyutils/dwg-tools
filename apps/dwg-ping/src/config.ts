b6ac1065e3c2f513a6a4514d5577e3dc2b14387a
HeinrichOlfert <heinrich.olf@gmail.com>
2023-12-11 16:38:06 +0100

    fix test interval min

--- apps/dwg-ping/src/config.ts
+++ apps/dwg-ping/src/config.ts
@@ -3,5 +3,5 @@ import { getEnv, getRequiredEnv } from "@joyutils/dwg-utils";
 export const GRAPHQL_URL = "https://query.joystream.org/graphql";
 
 export const SOURCE_ID = getRequiredEnv("SOURCE_ID");
-export const TEST_INTERVAL_MIN = getEnv("TEST_INTERVAL_MIN", "1");
+export const TEST_INTERVAL_MIN = getEnv("TEST_INTERVAL_MIN", "5");
 export const SINGLE_RUN = getEnv("SINGLE_RUN", undefined) === "true";
