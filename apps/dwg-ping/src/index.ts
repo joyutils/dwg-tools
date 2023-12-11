9435759bc102dad504fe465c431da1b05f2bdc48
HeinrichOlfert <heinrich.olf@gmail.com>
2023-12-11 16:31:48 +0100

    add speed video download

--- apps/dwg-ping/src/index.ts
+++ apps/dwg-ping/src/index.ts
@@ -7,6 +7,7 @@ import { getOperatorsDataQueryDocument } from "./query.js";
 import { GetDistributorOperatorsQuery } from "./gql/graphql.js";
 import {
   DistributionOperatorStatus,
+  ExtendedBenchmarkResult,
   OperatorAvailabilityResult,
   SampleAssetTestResult,
 } from "./types.js";
@@ -16,6 +17,11 @@ import {
   SOURCE_ID,
   TEST_INTERVAL_MIN,
 } from "./config.js";
+import { runBenchmark } from "./benchmark.js";
+import { gatherTestObjects } from "./gql/testData";
+import { formatMbps, formatMs } from "./until.js";
+import { performSpeedTest } from "./speedtest.js";
+
 
 const packageJson = JSON.parse(
   await fs.readFile(new URL("../package.json", import.meta.url), "utf-8")
@@ -23,7 +29,10 @@ const packageJson = JSON.parse(
 const packageVersion = packageJson.version;
 const userAgent = `dwg-ping/${packageVersion}`;
 const TEST_ASSET_ID = "1343";
-
+const ResultTime = 1;
+const TESTS_COUNT = 1;
+const chunkSize = 5 * 1e6;
+const uid = "3ee2cf1d-e10c-421a-9b07-6dcfb4bdd5d4"
 const esClient = await ElasticClient.initFromEnv();
 
 async function sendResults(results: OperatorAvailabilityResult[]) {
@@ -40,18 +49,86 @@ async function sendResults(results: OperatorAvailabilityResult[]) {
   }
 }
 
+async function sendVideoResult(results: ExtendedBenchmarkResult[]) {
+  const body = results.flatMap((result) => [
+    { index: { _index: "distributors-status" } },
+    result,
+  ]);
+  try {
+    await esClient.nativeClient.bulk({ body });
+    console.log("Sent the results to Elasticsearch");
+  } catch (e) {
+    console.error("Failed to send results to Elasticsearch");
+    console.error(e);
+  }
+}
+
+let videoTestCount = 0;
+
 async function runTest() {
   console.log(`Running test at ${new Date()}`);
+
   try {
+
     const operators = await getDistributionOperators();
+
     const results = await Promise.all(
       operators.map((operator) => getOperatorStatus(operator))
     );
+
     const resultsWithDegradations = await findOperatorDegradations(results);
 
     await sendResults(resultsWithDegradations);
 
+    const videoResults: ExtendedBenchmarkResult[] = [];
+
     console.log(JSON.stringify(resultsWithDegradations, null, 2));
+
+    videoTestCount++;
+
+    if (videoTestCount === ResultTime) {
+
+      const testObjects = await gatherTestObjects("270397");
+
+      if (!testObjects) {
+        console.log("Failed to prepare test data")
+        return;
+      }
+
+      const { referenceDownloadSpeedBps, referenceLatency } = await performSpeedTest();
+      console.log(testObjects)
+      for (const testObject of testObjects) {
+        for (const url of testObject.urls) {
+
+          const videoResult = await runBenchmark(url, chunkSize, TESTS_COUNT);
+          const extendedResult: ExtendedBenchmarkResult = {
+            ...videoResult,
+            objectType: testObject.type,
+            uid,
+            referenceDownloadSpeedBps,
+            referenceLatency,
+            version: "0.2.0",
+          };
+          if (videoResult.status === "success") {
+            console.log(
+              url,
+              formatMbps(videoResult.downloadSpeedBps),
+              formatMs(videoResult.ttfb)
+            );
+          } else {
+            console.log(extendedResult);
+          }
+
+          videoResults.push(extendedResult);
+        }
+      }
+
+      await sendVideoResult(videoResults);
+
+      console.log(videoResults)
+      videoTestCount = 0;
+    }
+
   } catch (e) {
     console.error("Test failed");
     console.error(e);
