d93aa920b5387460fb467076ebb90aad17d79691
HeinrichOlfert <heinrich.olf@gmail.com>
2023-12-13 00:20:17 +0900

    test commit

--- apps/dwg-ping/src/gql/index.ts
+++ apps/dwg-ping/src/gql/index.ts
@@ -1,3 +1,2 @@
 export * from "./fragment-masking.js";
 export * from "./gql.js";
-export * from "./videoGql.js"
--- apps/dwg-ping/src/index.ts
+++ apps/dwg-ping/src/index.ts
@@ -18,7 +18,7 @@ import {
   TEST_INTERVAL_MIN,
 } from "./config.js";
 import { runBenchmark } from "./benchmark.js";
-import { gatherTestObjects } from "./gql/testData";
+import { gatherTestObjects } from "./testData.js";
 import { formatMbps, formatMs } from "./until.js";
 import { performSpeedTest } from "./speedtest.js";
 
@@ -35,21 +35,7 @@ const chunkSize = 5 * 1e6;
 const uid = "3ee2cf1d-e10c-421a-9b07-6dcfb4bdd5d4"
 const esClient = await ElasticClient.initFromEnv();
 
-async function sendResults(results: OperatorAvailabilityResult[]) {
-  const body = results.flatMap((result) => [
-    { index: { _index: "distributors-status" } },
-    result,
-  ]);
-  try {
-    await esClient.nativeClient.bulk({ body });
-    console.log("Sent the results to Elasticsearch");
-  } catch (e) {
-    console.error("Failed to send results to Elasticsearch");
-    console.error(e);
-  }
-}
-
-async function sendVideoResult(results: ExtendedBenchmarkResult[]) {
+async function sendResults(results: OperatorAvailabilityResult[] | ExtendedBenchmarkResult[]) {
   const body = results.flatMap((result) => [
     { index: { _index: "distributors-status" } },
     result,
@@ -86,48 +72,41 @@ async function runTest() {
 
     videoTestCount++;
 
-    if (videoTestCount === ResultTime) {
+    // if (videoTestCount === ResultTime) {
 
-      const testObjects = await gatherTestObjects("270397");
+    //   // const testObjects = await gatherTestObjects("270397");
 
-      if (!testObjects) {
-        console.log("Failed to prepare test data")
-        return;
-      }
+    //   const { referenceDownloadSpeedBps, referenceLatency } = await performSpeedTest();
 
-      const { referenceDownloadSpeedBps, referenceLatency } = await performSpeedTest();
-      console.log(testObjects)
-      for (const testObject of testObjects) {
-        for (const url of testObject.urls) {
+    //   for (const url of testObjects.urls) {
+    //     const videoResult = await runBenchmark(url, chunkSize, TESTS_COUNT);
+    //     const extendedResult: ExtendedBenchmarkResult = {
+    //       ...videoResult,
+    //       objectType: testObjects.type,
+    //       uid,
+    //       referenceDownloadSpeedBps,
+    //       referenceLatency,
+    //       version: "0.2.0",
+    //     };
+    //     if (videoResult.status === "success") {
+    //       console.log(
+    //         url,
+    //         formatMbps(videoResult.downloadSpeedBps),
+    //         formatMs(videoResult.ttfb)
+    //       );
+    //     } else {
+    //       console.log(extendedResult);
+    //     }
 
-          const videoResult = await runBenchmark(url, chunkSize, TESTS_COUNT);
-          const extendedResult: ExtendedBenchmarkResult = {
-            ...videoResult,
-            objectType: testObject.type,
-            uid,
-            referenceDownloadSpeedBps,
-            referenceLatency,
-            version: "0.2.0",
-          };
-          if (videoResult.status === "success") {
-            console.log(
-              url,
-              formatMbps(videoResult.downloadSpeedBps),
-              formatMs(videoResult.ttfb)
-            );
-          } else {
-            console.log(extendedResult);
-          }
+    //     videoResults.push(extendedResult);
+    //   }
 
-          videoResults.push(extendedResult);
-        }
-      }
 
-      await sendVideoResult(videoResults);
+    //   await sendResults(videoResults);
 
-      console.log(videoResults)
-      videoTestCount = 0;
-    }
+    //   console.log(videoResults)
+    //   videoTestCount = 0;
+    // }
 
   } catch (e) {
     console.error("Test failed");
@@ -138,6 +117,7 @@ async function runTest() {
 async function getOperatorStatus(
   operator: GetDistributorOperatorsQuery["distributionBucketOperators"][0]
 ): Promise<OperatorAvailabilityResult> {
+
   const distributingStatus: OperatorAvailabilityResult["distributingStatus"] =
     operator.distributionBucket.distributing
       ? "distributing"
